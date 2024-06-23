16b2555d519c5a919c7bc41f2cf68649ffb9c8d3
Abdulrehman <you@example.com>
2024-06-23 10:35:37 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,30 @@
+
+
+// Green alien version
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You earned 5 points.");
+} else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+} else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
+// Yello alien version
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You earned 5 points.");
+} else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+} else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
+// Red alien version
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("You earned 5 points.");
+} else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+} else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
+
